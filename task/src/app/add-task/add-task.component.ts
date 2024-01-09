6e62458943d2feee79e9f9867f4106c4b4b386e7
hassanhashmi77 <hassanhashmi7790@gmail.com>
2024-01-09 16:51:48 +0500

    add clock time functionality and some design

---
+++ task/src/app/add-task/add-task.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  hours: number = 0
+  minuts: number = 0
+  second: number = 0
+  ngOnInit(): void {
+    this.getTime()
+  }
+
+  getTime() {
+    setInterval(() => {
+      var date = new Date();
+      this.hours = date.getHours()
+      this.minuts = date.getMinutes()
+      this.second = date.getSeconds()
+      // console.log(this.hours,this.minuts,this.second )
+    }, 1000);
+  }
+
+}
--- task/src/app/app.module.ts
+++ task/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
